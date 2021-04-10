b18c79d1ead106c8250d1c85bbadf493bfd72212
Oleksandr Bieliakov <abel9k@gmail.com>
2021-04-10 13:03:25 +0200

    multi-class classifier using the perceptrons implemented

--- project2/perceptron.py
+++ project2/perceptron.py
@@ -3,33 +3,92 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
 from plotka import plot_decision_regions
+from plotka import my_plot
 
 
 class Perceptron(object):
 
-    def __init__(self, eta=0.01, n_iter=10):
+    def __init__(self, eta=0.01, n_iter=1000):
         self.eta = eta
         self.n_iter = n_iter
 
-    def fit(self, X, y):
-        self.w_ = np.zeros(1+ X.shape[1])
+    def fit(self, x, y):
+        self.w_ = np.zeros(1 + x.shape[1])
         self.errors_ = []
 
         for _ in range(self.n_iter):
             errors = 0
-            for xi, target in zip(X,y):
+            for xi, target in zip(x, y):
                 update = self.eta * (target - self.predict(xi))
-                self.w_[1:] += update *xi
+                self.w_[1:] += update * xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
             self.errors_.append(errors)
         return self
 
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+    def net_input(self, x):
+        return np.dot(x, self.w_[1:]) + self.w_[0]
 
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, -1)
+    def predict(self, x):
+        return np.where(self.net_input(x) >= 0.0, 1, -1)
+
+
+def get_data():
+    iris = datasets.load_iris()
+    x = iris.data[:, [2, 3]]
+    y = iris.target
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.7, random_state=1, stratify=y)
+    return x_train, x_test, y_train, y_test
+
+
+def train_perceptron(train_data, train_data_classes_original, class_value):
+    train_data_classes = train_data_classes_original.copy()
+    train_data_classes[(train_data_classes != class_value)] = -1
+    train_data_classes[(train_data_classes == class_value)] = 1
+    ppn = Perceptron()
+    ppn.fit(train_data, train_data_classes)
+    return ppn
+
+
+def train_perceptrons(train_data, train_data_classes):
+    perceptrons = []
+    for class_value in np.unique(train_data_classes):
+        perceptron = train_perceptron(train_data, train_data_classes, class_value)
+        perceptron.class_value = class_value
+        perceptrons.append(perceptron)
+    return perceptrons
+
+
+# returns true if perceptron was activated
+def test_sample(sample, perceptron):
+    return perceptron.predict(sample) == 1
+
+
+def classify(perceptrons, test_data):
+    predictions = []
+    for sample in test_data:
+        activated_classes = []
+        for perceptron in perceptrons:
+            if test_sample(sample, perceptron):
+                activated_classes.append(perceptron.class_value)
+        predictions.append(activated_classes)
+    return predictions
+
+
+def plot(data, classes):
+    my_plot(x=data, y=classes)
+    plt.xlabel(r'$x_1$')
+    plt.ylabel(r'$x_2$')
+    plt.legend(loc='upper left')
+    plt.show()
+
+
+def test():
+    train_data, test_data, train_data_classes, test_data_classes = get_data()
+    perceptrons = train_perceptrons(train_data, train_data_classes)
+    predictions = classify(perceptrons, test_data)
+    plot(test_data, test_data_classes)
+    plot(test_data, predictions)
 
 
 def main():
@@ -41,7 +100,7 @@ def main():
 
     X_train_01_subset = X_train[(y_train == 0) | (y_train == 1)]
     y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
-    # w perceptronie wyjście jest albo 1 albo -1    
+    # w perceptronie wyjście jest albo 1 albo -1
     y_train_01_subset[(y_train_01_subset == 0)] = -1
     ppn = Perceptron(eta=0.1, n_iter=10)
     ppn.fit(X_train_01_subset, y_train_01_subset)
@@ -54,4 +113,4 @@ def main():
     
 
 if __name__ == '__main__':
-    main()
+    test()
--- project2/plotka.py
+++ project2/plotka.py
@@ -3,24 +3,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+def plot_decision_regions(x, y, classifier, test_idx=None, resolution=0.02):
+    # konfiguruje generator znaczników i mapę kolorów
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
 
-        # konfiguruje generator znaczników i mapę kolorów
-        markers = ('s', 'x', 'o', '^', 'v')
-        colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-        cmap = ListedColormap(colors[:len(np.unique(y))])
+    # rysuje wykres powierzchni decyzyjnej
+    x1_min, x1_max = x[:, 0].min() - 1, x[:, 0].max() + 1
+    x2_min, x2_max = x[:, 1].min() - 1, x[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
 
-        # rysuje wykres powierzchni decyzyjnej
-        x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-        x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-        Z = Z.reshape(xx1.shape)
-        plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
-        plt.xlim(xx1.min(), xx1.max())
-        plt.ylim(xx2.min(), xx2.max())
+    # rysuje wykres wszystkich próbek
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=x[y == cl, 0], y=x[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl,
+                    edgecolor='black')
 
-        # rysuje wykres wszystkich próbek
-        for idx, cl in enumerate(np.unique(y)):
-            plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl, edgecolor='black')
 
+def my_plot(x, y):
+    markers = ('s', 'x', 'o', '^', 'v', '8', '4')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan', 'yellow', 'fuchsia')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    for idx, cl in enumerate(np.unique(y)):
+        selection = []
+        for value in y:
+            if value == cl:
+                selection.append(True)
+            else:
+                selection.append(False)
+        plt.scatter(x=x[selection, 0], y=x[selection, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl,
+                    edgecolor='black')
